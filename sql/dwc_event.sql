046a7c473fe478816a9c0b5997a45ee9c20d9a51
peterdesmet <peter.desmet@inbo.be>
2022-03-16 16:12:19 +0100

    Start dwc_event

--- sql/dwc_event.sql
+++ sql/dwc_event.sql
@@ -2,8 +2,29 @@
 Created by Peter Desmet (INBO)
 */
 SELECT
-  *
+-- RECORD-LEVEL
+-- basisOfRecord
+  'HumanObservation' AS basisOfRecord,
+
+-- EVENT
+-- eventID
+  c.CampaignID || ':' || s.SampleID || ':' || p.PositionID AS eventID,
+-- parentEventID
+  c.CampaignID || ':' || s.SampleID AS parentEventID,
+-- eventDate: p.Time is in UTC
+  s.Date || 'T' || p.Time || 'Z' AS eventDate,
+
+-- LOCATION
+-- decimalLatitude
+  p.Latitude AS decimalLatitude,
+-- decimalLongitude
+  p.Longitude AS decimalLongitude,
+-- geodeticDatum
+  'EPSG:4326' AS geodeticDatum
+
 FROM
-  campaigns
-LIMIT
-  1
+  campaigns AS c
+  LEFT JOIN samples AS s
+    ON c.CampaignID = s.CampaignID
+  LEFT JOIN positions AS p
+    ON s.SampleID = p.SampleID
