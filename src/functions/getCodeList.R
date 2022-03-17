c21167d2c8e77aef462eed6ab98dc4e2fabfcac6
peterdesmet <peter.desmet@inbo.be>
2022-03-17 12:21:40 +0100

    Document function

--- src/functions/getCodeList.R
+++ src/functions/getCodeList.R
@@ -1,3 +1,11 @@
+#' Get code list
+#'
+#' Returns all codes for the given code type as a data frame. Uses the
+#' [`GetCodeList`](http://vocab.ices.dk/services/POX.aspx#GetCodeList) API.
+#'
+#' @param codeType Code type, e.g. `SEXCO`.
+#' @return A data frame with all codes for that code type. Will return an empty
+#'   data frame for unrecognized code type.
 getCodeList <- function(codeType) {
   endpoint <- paste0("https://vocab.ices.dk/services/pox/GetCodeList/", codeType)
   result <- xml2::read_xml(endpoint)
