c3a3d84aee290aa2aacc3e49131857c93cef9219
peterdesmet <peter.desmet@inbo.be>
2022-03-17 17:06:41 +0100

    Add shipc as emof

--- sql/dwc_emof.sql
+++ sql/dwc_emof.sql
@@ -2,9 +2,30 @@
 Created by Peter Desmet (INBO)
 */
 SELECT
-  *
+-- eventID
+  c.CampaignID || ':' || s.SampleID AS eventID,
+-- measurementID
+-- occurrenceID
+-- measurementType
+  'platform' AS measurementType,
+-- measurementTypeID
+  'http://vocab.nerc.ac.uk/collection/C17/current/' AS measurementTypeID,
+-- measurementValue
+  shipc.Description AS measurementValue,
+-- measurementValueID
+  'http://vocab.nerc.ac.uk/collection/C17/current/' || shipc.Key AS measurementValueID,
+-- measurementUnit
+  NULL as measurementUnit,
+-- measurementUnitID
+  NULL as measurementUnitID
 
 FROM
-  campaigns
+  samples AS s
+  LEFT JOIN campaigns AS c
+    ON s.CampaignID = c.campaignID
+  LEFT JOIN shipc AS shipc
+    ON s.PlatformCode = shipc.Key
 
-LIMIT {limit}
+WHERE
+  c.CampaignID = {campaign_id}
+
