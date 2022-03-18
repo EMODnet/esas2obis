f3f453ec667942195a414df7ac7ab0efba42deee
peterdesmet <peter.desmet@inbo.be>
2022-03-18 13:24:11 +0100

    Add platform class

--- sql/dwc_emof.sql
+++ sql/dwc_emof.sql
@@ -10,7 +10,7 @@ SELECT
 -- occurrenceID
   NULL AS occurrenceID,
 -- measurementType
-  'platform' AS measurementType,
+  'platform code' AS measurementType,
 -- measurementTypeID
   'http://vocab.nerc.ac.uk/collection/C17/current/' AS measurementTypeID,
 -- measurementValue
@@ -30,3 +30,31 @@ FROM
 WHERE
   c.CampaignID = {campaign_id}
 
+UNION
+
+/* SAMPLE: PLATFORM CLASS */
+SELECT
+-- eventID
+  c.CampaignID || ':' || s.SampleID AS eventID,
+-- occurrenceID
+  NULL AS occurrenceID,
+-- measurementType
+  'platform class' AS measurementType,
+-- measurementTypeID
+  'http://vocab.ices.dk/?ref=311' AS measurementTypeID,
+-- measurementValue
+  platformclass.Description AS measurementValue,
+-- measurementValueID
+  NULL AS measurementValueID, -- TODO
+-- measurementUnit
+  NULL as measurementUnit,
+-- measurementUnitID
+  NULL as measurementUnitID
+FROM
+  samples AS s
+  LEFT JOIN campaigns AS c
+    ON s.CampaignID = c.campaignID
+  LEFT JOIN platformclass AS platformclass
+    ON s.PlatformClass = platformclass.Key
+WHERE
+  c.CampaignID = {campaign_id}
